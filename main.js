0e538f3fe2121c6ef9ea9754bb09fa252158f4b8
aaronprasla <52900151+aaronprasla@users.noreply.github.com>
2022-01-17 11:49:18 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-function preload() {
+nosex = 0;
+nosey = 0;
 
+function preload() {
+	mustache = loadImage('https://i.postimg.cc/GmrFXT6w/handlebar-moustache-clip-art-mustache-removebg-preview.png');
 }
 
 function draw() {
 	image(video, 0, 0, 300, 300);
+	image(mustache, nosex, nosey, 100, 50);
 }
 
 function setup() {
@@ -19,7 +23,7 @@ function setup() {
 }
 
 function take_snapshot() {
-	save('Face_Filter_img.png')
+	save('Mustache.png')
 }
 
 function modelLoaded() {
@@ -31,5 +35,8 @@ function gotPoses(results) {
 		console.log(results);
 		console.log('x pos = ' + results[0].pose.nose.x);
 		console.log('y pos = ' + results[0].pose.nose.y);
+
+		nosex = (results[0].pose.nose.x) - 50;
+		nosey = (results[0].pose.nose.y) - 2;
 	}
 }
